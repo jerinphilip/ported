f331afa14beab001bceefc3d704b5dfb8ce29dbf
Jerin Philip <monu1618@gmail.com>
2016-08-11 16:39:38 +0530

    python2 + payyans
    python2 compatibility, unpack function added.
    Payyans added.

--- app.py
+++ app.py
@@ -3,14 +3,21 @@ from wsmeext.flask import signature
 import flask
 import pkg_resources
 
+def unpack(data):
+    print(data)
+    func = data[0]
+    args = data[1:-1]
+    rettype = data[-1]
+    #func, args, rettype = data
+    return (func, args, rettype)
 
 def main():
     app = flask.Flask(__name__)
     app.config['DEBUG'] = True
     for entrypoint in pkg_resources.iter_entry_points('libindic.api.rest'):
-    #for entrypoint in pkg_resources.iter_entry_points('myapp.api.rest'):
-        func, *args, rettype = entrypoint.load()()
-        print(func.__name__, rettype, args)
+        print(entrypoint.load()())
+        func, args, rettype = unpack(entrypoint.load()())
+        #print(func.__name__, rettype, args)
         app.route('/' + func.__name__, methods=['POST', 'GET'])(
         signature(rettype, *args)(func))
         #app.route('/' + func.__name__, methods=['GET'])(
